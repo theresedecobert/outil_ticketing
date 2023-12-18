996546526d2df8ee219b6de7f169512108ffc3a1
theresedecobert <theresedecobert@gmail.com>
2023-12-18 14:10:43 +0100

    création entity/crud

---
+++ outil_ticketing/src/Entity/Files.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FilesRepository::class)]
+class Files
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $file = null;
+
+    #[ORM\ManyToOne(inversedBy: 'files')]
+    private ?Tickets $ticket = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): static
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function getTicket(): ?Tickets
+    {
+        return $this->ticket;
+    }
+
+    public function setTicket(?Tickets $ticket): static
+    {
+        $this->ticket = $ticket;
+
+        return $this;
+    }
+}
