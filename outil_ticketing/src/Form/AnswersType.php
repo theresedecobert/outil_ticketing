5d17160fed359f2829eb7549d9c28075e8216878
theresedecobert <theresedecobert@gmail.com>
2023-12-19 10:38:45 +0100

    modification des formulaires

--- outil_ticketing/src/Form/AnswersType.php
+++ outil_ticketing/src/Form/AnswersType.php
@@ -3,20 +3,44 @@
 namespace App\Form;
 
 use App\Entity\Answers;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AnswersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
+            ->add('description', TextareaType::class, [
+                'label'=>'Description',
+                'attr' => ['rows' => '10',
+                'class' => 'custom-form'
+            ], ] )
             ->add('resoluted_at')
-            ->add('docLink')
-            ->add('user')
-            ->add('ticket')
+            ->add('docLink', TextType::class, [
+                'label' => 'Lien',
+                'attr' => [
+                    'class' => 'custom-form',
+                ],
+            ])
+            ->add('user', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'custom-form',
+                ],
+            ])
+            ->add('ticket', 
+            TextType::class, [
+                'label' => 'Nom du ticket',
+                'attr' => [
+                    'class' => 'custom-form',
+                ],
+            ])
+            
         ;
     }
 
--- outil_ticketing/src/Form/UserType.php
+++ outil_ticketing/src/Form/UserType.php
@@ -6,18 +6,60 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
-        ;
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'label' => 'E-mail',
+                    'attr' => [
+                        'class' => 'custom-form'
+                    ],
+                ]
+            )
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => true, // pour afficher les choix sous forme de cases à cocher
+            ])
+            ->add('password', TextType::class, [
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'class' => 'custom-form',
+                ],
+            ])
+            ->add(
+                'firstName',
+                TextType::class,
+                [
+                    'label' => 'Prénom',
+                    'attr' => [
+                        'class' => 'custom-form',
+                    ],
+                ]
+            )
+            ->add(
+                'lastName',
+                TextType::class,
+                [
+                    'label' => 'Nom',
+                    'attr' => [
+                        'class' => 'custom-form',
+                    ],
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
